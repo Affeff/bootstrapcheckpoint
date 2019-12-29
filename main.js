d787badafbc7c2344e92195d0ac2a066cac1e90e
Affeff <57836061+Affeff@users.noreply.github.com>
2019-12-29 18:14:51 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function changeItBold():{
+var textstate=document.getElementById('MyText').style.fontWeight;
+{
+if textstate!='bold'
+	document.getElementById('MyText').style.fontWeight='bold';
+}else{
+	document.getElementById('MyText').style.fontWeight='normal';
+}
+}
+
+function changeItItalic():{
+	var textstaty=document.getElementById('Mytext').style.fontStyle;
+{
+	if textstaty!='italic'
+		document.getElementById('Mytext').style.fontStyle=italic;
+}else{
+	document.getElementById('Mytext').style.fontStyle=normal;
+}
+}
+function changeItUnderline():{
+	var textdeco=document.getElementById('Mytext').style.textDecoration;
+}
+if textdeco
